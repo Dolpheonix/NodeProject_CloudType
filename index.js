4dc8b1e392c4b9b5cbf67e714a10b92cd2ff59d7
Dolpheonix <moonfog97@gmail.com>
2025-02-03 20:42:20 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cookieparser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieparser());
+
+app.get('/', (req, res) => {
+    const msg = {
+        message: 'hello myapp',
+        index: 0
+    };
+    res.send(JSON.stringify(msg));
+});
+
+app.listen(3000, ()=> {
+    console.log("Connected");
+})
